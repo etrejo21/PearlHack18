267078081fe736eae2c35169b82ef2c386b20e44
Lillian Zhang <lzhang375@gatech.edu>
2018-02-10 14:45:12 -0500

    created start screen files

---
+++ src/pages/start/startscreen.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'startscreen-about',
+  templateUrl: 'startscreen.html'
+})
+export class StartScreenPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
